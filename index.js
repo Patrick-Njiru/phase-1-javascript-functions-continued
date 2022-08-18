81aa1e9c435dfc8dcdc7e560df05ab07b9be1290
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-08-18 13:35:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // code your solution here
+function saturdayFun(activity) {
+    if ( activity) {
+        return `This Saturday, I want to ${activity}!`
+    }
+    else {
+        return 'This Saturday, I want to roller-skate!'
+    }
+}
+
+function mondayWork(activity) {
+    if (activity) {
+        return `This Monday, I will ${activity}.`
+    }
+    else {
+        return "This Monday, I will go to the office."
+    }
+}
+
+function wrapAdjective(initializer) {
+   return function () {
+    if (initializer === "||") {
+        let result = '||a dedicated programmer||'
+        let emphatic = `You are ${result}!`
+        return emphatic
+    }
+    else if (initializer === '*') {
+        let  result = '*a hard worker*'
+        let emphatic = `You are ${result}!`
+        return emphatic
+    }
+   }
+}
+console.log(wrapAdjective('*'));
